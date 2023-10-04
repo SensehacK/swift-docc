dd1ca0d5e6136a2dd7742585dd086010eabaed69
Kautilya <kautilyasave@gmail.com>
2023-10-04 16:19:31 -0600

    :ship: it

--- Sources/swifty-docc/swifty_docc.swift
+++ Sources/swifty-docc/swifty_docc.swift
@@ -1,12 +1,17 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-/// This is a trials place for docummentation checks.
+/// This is a trials place for documentation checks.
 public class Trial {
     
-    /// This is another docusa Kautilya Save
+    /// This is another docs Kautilya Save
     public init() {
         print("Hello Sensehack!")
+    }
+    
+    /// We don't believe in local testing only production testing.
+    public shipIt() {
+        print("Give the intern the production rights and watch the architects cry!")
     }
     
 }
